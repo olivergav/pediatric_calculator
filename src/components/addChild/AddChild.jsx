dfe337fd00bcb760f7664d856d391da41ad3bc94
olivergav <oliwier.gawlik0@gmail.com>
2022-05-19 21:23:35 +0200

    AddChild functions for validation and add useEffect to set BMI

--- src/components/addChild/AddChild.jsx
+++ src/components/addChild/AddChild.jsx
@@ -1,17 +1,42 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import boy_avatar from "./../../images/boy_avatar.png"
+import girl_avatar from "./../../images/girl_avatar.png"
 
 function AddChild(){
     const [childName, setChildName] = useState("");
+    const [gender, setGender] = useState("");
+    const [weight, setWeight] = useState("");
+    const [height, setHeight] = useState("");
+    const [bmi, setBMI] = useState("");
+
+    useEffect(() => {
+        if (height > 0 && weight > 0){
+            setBMI(Math.round(weight / ((height / 100) ** 2)))
+        }
+    }, [height, weight])
+
 
     function validateChildName(e) {
-        setChildName(e.target.value)
+        setChildName(e.target.value);
+    }
+
+    function handleGender(e) {
+        setGender(e.target.value);
+    }
+
+    function handleWeight(e) {
+        setWeight(Math.abs(e.target.value))
+    }
+
+    function handleHeight(e) {
+        setHeight(Math.abs(e.target.value))
     }
 
     return (
         <div>
             <form>
                 <div>
-                    <img src="" alt="Child"/>
+                    <img src={gender === "male" ? boy_avatar : girl_avatar} alt="Child"/>
                     <input type="text" placeholder="Imię" value={childName} onChange={validateChildName}/>
                 </div>
 
@@ -19,27 +44,28 @@ function AddChild(){
                 <input type="date" id="birthday"/>
 
                 <label htmlFor="weight">Waga:</label>
-                <input type="text" id="weight"/>
+                <input type="number" id="weight" value={weight} min={0}
+                       onChange={handleWeight} placeholder={'Waga'}/>
 
                 <label htmlFor="height">Wzrost:</label>
-                <input type="text" id="height"/>
+                <input type="number" id="height" value={height} min={0}
+                       onChange={handleHeight} placeholder={'Wzrost'}/>
 
                 <div>
-                    <label htmlFor="sex">Płeć:</label>
+                    <label htmlFor="gender">Płeć:</label>
 
                     <div>
                         <label htmlFor="female">Kobieta</label>
-                        <input type="radio" id="female" value="female"/>
+                        <input type="radio" name="gender" id="female" value="female" onChange={handleGender}/>
                     </div>
-
                     <div>
                         <label htmlFor="male">Mężczyzna</label>
-                        <input type="radio" id="male" value="male"/>
+                        <input type="radio" name="gender" id="male" value="male" onChange={handleGender}/>
                     </div>
                 </div>
 
                 <label htmlFor="bmi">BMI</label>
-                <input type="text"/>
+                <input type="text" id="bmi" value={bmi} disabled/>
             </form>
         </div>
     )
